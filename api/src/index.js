b52ed241a0708695c2e4fc36c5d6110415927c50
LepharamRamchiary <lepharamchiary@gmail.com>
2024-12-02 19:20:42 +0530

    add express.

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world')
+import express from "express";
+import dotene from "dotenv";
+dotene.config();
+
+const port = process.env.PORT || 8001;
+
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Server is live on: ${port}`);
+    
+})
